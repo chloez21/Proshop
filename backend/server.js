a7949b6df7760c2cd9292bf308042a4ef4b139f8
Chloe <art.cchu@gmail.com>
2022-03-09 09:45:40 -0500

    Custom Error Handling

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
 
@@ -11,10 +12,13 @@ connectDB()
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('API is running.......')
+    res.send('API is running....')
 })
 
 app.use('/api/products', productRoutes)//会被link到productRoutes
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
